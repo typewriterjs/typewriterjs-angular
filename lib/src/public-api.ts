d86f9bb894bccb32b8920137b22a6c5e33dd9197
codemile <nick@reactgular.com>
2019-04-30 17:49:28 -0400

    Restructred folders

---
+++ lib/src/public-api.ts
@@ -0,0 +1,2 @@
+export * from './core/render-buffer/render-buffer.component';
+export * from './core/rg-animated-typing.module';
--- projects/rg-animated-typing/src/public-api.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/render-buffer/render-buffer.component';
-export * from './lib/rg-animated-typing.module';
